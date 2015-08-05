7cd61afebe8d440ae4632ba1e617662a5c81d702
Beth Hoskins <bhoskins@charter.net>
2015-08-05 14:15:34 -0400

    Initial commit

---
+++ scripts/app.js
@@ -0,0 +1,17 @@
+ (function(){
+
+  'use strict';
+  $(document).ready(function(){
+
+    console.log('im ready');
+    //to initate tabs
+    $('#nav-tabs a:first').tab('show');
+
+// initialize tool tips
+    $('[data-toggle="tooltip"]').tooltip();
+
+
+  });
+
+
+ })();
